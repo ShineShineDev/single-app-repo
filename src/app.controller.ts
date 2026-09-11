66a4a278560451803ab8ba4d7ebd128f5d7b3fb3
Spidey Shine <aungshine194@gmai.com>
2026-09-11 15:48:18 +0630

    some changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,6 +14,7 @@ export class AppController {
 
   @Get('health')
   getHealth(): { status: string; uptime: number; timestamp: string } {
+    const test = "TEST"
     return this.appService.getHealth();
   }
 }
